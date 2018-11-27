19f1b5bea2936a8d1a73f3dac3b7faf6f0b44038
Jingnan Shi <jshi@g.hmc.edu>
2018-11-27 13:00:49 -0800

    tested on hardware; small modifications to behaviors

--- useless_boxes.sv
+++ useless_boxes.sv
@@ -1,4 +1,5 @@
 // Jingnan Shi
+// useless_boxes.sv
 // Nov. 17, 2018
 module useless_boxes(input logic clk,
 						   input logic reset,
@@ -32,7 +33,7 @@ module useless_box(input logic clk,
 	
 	// Behaviors
 	logic [19:0] behavior_1, behavior_2, behavior_3;
-	assign behavior_1 = 20'd18000; // push the switch back
+	quickpush behav1(clk, switch_pulse, behavior_1);
 	slowpush behav2(clk, switch_pulse, behavior_2);
 	slow_then_fast_push behav3(clk, switch_pulse, behavior_3);
 		
@@ -69,19 +70,18 @@ module useless_box(input logic clk,
 			 
 endmodule 
 
-// Slowly push the switch
-module slowpush(input logic clk,
+// Quick push
+module quickpush(input logic clk,
 					 input logic reset,
 					 output logic [19:0] duty_cycle);
 	 
-	// A counter to count from 3.2 ms (128000) to 0.45 ms (18000)
 	// ~10 Hz update rate of output duty_cycle
-	logic [3:0] q;
+	logic [21:0] q;
 	logic sclk;
-	assign sclk = q[3];
+	assign sclk = q[21];
 	always_ff @(posedge clk, posedge reset) begin
-		if (reset) q <= 4'b0;
-		else q <= q + 4'b1;
+		if (reset) q <= 22'b0;
+		else q <= q + 22'b1;
 	end
 	
 	// sweep the angle
@@ -89,8 +89,36 @@ module slowpush(input logic clk,
 		if (reset) begin
 			duty_cycle <= 20'd128000;
 		end else begin
-			if (duty_cycle > 20'd18000) duty_cycle <= duty_cycle - 20'd2200; //50 steps
-			else duty_cycle <= duty_cycle;
+			if (duty_cycle > 20'd100000) duty_cycle <= duty_cycle - 20'd1400; //20 steps
+			else duty_cycle <= 20'd60000;
+		end 
+	end
+	
+endmodule
+
+
+// Slowly push the switch
+module slowpush(input logic clk,
+					 input logic reset,
+					 output logic [19:0] duty_cycle);
+	 
+	// A counter to count from 3.2 ms (128000) to 0.45 ms (18000)
+	// ~10 Hz update rate of output duty_cycle
+	logic [21:0] q;
+	logic sclk;
+	assign sclk = q[21];
+	always_ff @(posedge clk, posedge reset) begin
+		if (reset) q <= 22'b0;
+		else q <= q + 22'b1;
+	end
+	
+	// sweep the angle
+	always @(posedge sclk, posedge reset) begin
+		if (reset) begin
+			duty_cycle <= 20'd128000;
+		end else begin
+			if (duty_cycle > 20'd70000) duty_cycle <= duty_cycle - 20'd1160; //50 steps
+			else duty_cycle <= 20'd60000;
 		end 
 	end
 	
@@ -104,12 +132,12 @@ module slow_then_fast_push(input logic clk,
 	// A counter to count from 3.2 ms (128000) to 2 ms (80000)
 	// then jump to 0.45 ms (18000)
 	// ~10 Hz update rate of output duty_cycle [21:0]
-	logic [3:0] q;
+	logic [21:0] q;
 	logic sclk;
-	assign sclk = q[3];
+	assign sclk = q[21];
 	always_ff @(posedge clk, posedge reset) begin
-		if (reset) q <= 4'b0;
-		else q <= q + 4'b1;
+		if (reset) q <= 22'b0;
+		else q <= q + 22'b1;
 	end
 	
 	// sweep the angle
@@ -117,7 +145,7 @@ module slow_then_fast_push(input logic clk,
 		if (reset) duty_cycle <= 20'd128000;
 		else begin
 			if (duty_cycle > 20'd80000) duty_cycle <= duty_cycle - 20'd2400;  //20 steps 
-		   else duty_cycle <= 20'd18000;
+		   else duty_cycle <= 20'd60000;
 		end
 	end
 	
